d0c09edf1a5377b1a0620b5e3cbe6ae14765b579
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-09-25 22:48:32 +0000

    Sun, Sep 25, 2022, 10:48 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,5 +5,9 @@ int main(void)
 {
     int cardNumber = get_long("Number: ");
     int numLength = floor(log10(abs(cardNumber)) + 1);
-    if (numLength < 13 || numLength == 14 || numLength > )
+    if (numLength < 13 || numLength == 14 || numLength > 16)
+    {
+        printf("INVALID");
+    }
+    printf()
 }
