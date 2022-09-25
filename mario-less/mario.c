8250bb925da4f11fd8bee77e04849d791ae17c14
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-09-25 21:57:20 +0000

    Sun, Sep 25, 2022, 9:57 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height = get_int("Height: ");
+    if (height <= 8 && height >= 1)
+    {
+
+    } else
+    {
+        
+    }
 }
