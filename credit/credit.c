a6bb48f7100d9136bfeaa0b6aa8f3091f20cdb82
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-09-28 19:08:53 +0000

    Wed, Sep 28, 2022, 7:08 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -28,7 +28,7 @@ int main(void)
     for (int i = 0; i < floor(numLength / 2));
     int digit = 0;
     {
-        digit = cardNumber % pow(100)
+        digit = cardNumber % pow(10)
         doubledNums[i] = digitArray[-2 * (i + 1)] * 2;
     }
 // get individual digits of doubled nums ???
