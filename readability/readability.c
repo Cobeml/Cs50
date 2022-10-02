aead91e297166989d1a1e62452cc0689801579c4
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-10-02 15:03:21 +0000

    Sun, Oct 2, 2022, 3:03 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,8 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// declare function
+long lettersPer100(string words);
+long sentencesPer100(string words);
+
 int main(void)
 {
+    // get user input
     string text = get_string("Text: ");
-    int index = 0.0588 * L(string text) - 0.296 * S(string text) - 15.8;
+    // find index from 
+    int index = round(0.0588 * lettersPer100(string text) - 0.296 * sentencesPer100(string text) - 15.8);
 }
