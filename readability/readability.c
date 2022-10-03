4d6471da7ccca8f84cbc396d15620e6551f3d779
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-10-03 22:52:55 +0000

    Mon, Oct 3, 2022, 10:52 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -11,7 +11,7 @@ int main(void)
     // get user input
     string text = get_string("Text: ");
     // find index from text
-    int index = round(0.0588 * lettersPer100(string text) - 0.296 * sentencesPer100(string text) - 15.8);
+    int index = round(0.0588 * lettersPer100(text) - 0.296 * sentencesPer100(text) - 15.8);
     // print output based on index
     if (index >= 16) {
         printf ("Grade 16+\n");
