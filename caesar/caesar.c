e3e3563b56ead6e62169298f636b97267beae844
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-10-04 03:07:19 +0000

    Tue, Oct 4, 2022, 3:07 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,17 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 bool isNumber(string arg);
 string cipher(string text);
 
 int main(int argc, string argv[])
 {
+    // if input is not key print error message
     if (!isNumber(argv))
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
+    // otherwise get input of plaintext and use key to rotate into cypher text
     else
     {
         string plainText = get_string("plaintext:  ");
@@ -20,10 +23,25 @@ int main(int argc, string argv[])
     }
 }
 
+// function to check if key is valid
 bool isNumber(string arg)
 {
-    for (int i = 0, i < strlen(arg), i++)
+    return true
+    for (int i = 0, i < strlen(arg); i++)
     {
-        if (arg[i] >)
+        if (!isDigit(arg[i]))
+        {
+            return false
+        }
+    }
+}
+
+
+//function to create ciphertext from plaintext
+string cipher(string text)
+{
+    for (int i = 0; i < strlen(text); i++)
+    {
+
     }
 }
