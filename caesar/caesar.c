8c4f0d44265928300ea2fa0b6feeae1f0caee129
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-10-04 02:55:16 +0000

    Tue, Oct 4, 2022, 2:55 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
+
 bool isNumber(string arg);
 string cipher(string text);
 
@@ -9,10 +10,13 @@ int main(int argc, string argv[])
     if (!isNumber(argv))
     {
         printf("Usage: ./caesar key\n");
+        return 1;
     }
     else
     {
         string plainText = get_string("plaintext:  ");
         string cipherText = cipher(plainText);
+        printf("ciphertext: %s\n", cipherText);
     }
 }
+
