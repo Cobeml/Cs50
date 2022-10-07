318fbb9622ccce5c42eb75b0d1f85835b38432dd
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-10-07 15:07:02 +0000

    Fri, Oct 7, 2022, 3:07 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -31,7 +31,6 @@ bool vote(int rank, string name, int ranks[]);
 void record_preferences(int ranks[]);
 void add_pairs(void);
 void sort_pairs(void);
-printf("Hello\n");
 void lock_pairs(void);
 void print_winner(void);
 
@@ -93,6 +92,7 @@ int main(int argc, string argv[])
 
     add_pairs();
     sort_pairs();
+    printf("%i%i\n%i%i\n%i%i\n", pairs[0].winner, pairs[0].loser, pairs[1].winner, pairs[1].loser, pairs[2].winner, pairs[2].loser);
     lock_pairs();
     print_winner();
     return 0;
