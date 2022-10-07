f1affbb0fd4be242c972cf87e20d5fba28a111b4
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-10-07 01:05:26 +0000

    Fri, Oct 7, 2022, 1:05 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -101,7 +101,7 @@ bool vote(int rank, string name, int ranks[])
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (name == candidate[i])
+        if (name == candidates[i])
         {
             ranks[rank] = i;
             return true;
