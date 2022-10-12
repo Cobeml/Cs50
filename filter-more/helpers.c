2acde62483416786d486467fc463277d5c1eb3a3
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-10-12 20:04:06 +0000

    Wed, Oct 12, 2022, 8:04 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -42,21 +42,23 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE pixel = image[i][j];
             RGBTRIPLE surrounding_pixels[9];
             surrounding_pixels[0] = image[i - 1][j - 1];
             surrounding_pixels[1] = image[i][j - 1];
             surrounding_pixels[2] = image[i - 1][j];
-            surrounding_pixels[3] = pixel;
+            surrounding_pixels[3] = image[i][j];
             surrounding_pixels[4] = image[i - 1][j + 1];
             surrounding_pixels[5] = image[i + 1][j - 1];
             surrounding_pixels[6] = image[i + 1][j];
             surrounding_pixels[7] = image[i][j + 1];
             surrounding_pixels[8] = image[i + 1][j + 1];
 
+            BYTE redavg = 0;
+            BYTE greenavg = 0;
+            BYTE 
             for (int h = 0; h < 9; h++)
             {
-                
+                surrounding_pixel[h]
             }
         }
     }
