4fe4455654ba10dc3a3d9a86461b168b1c7a3ca1
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-10-12 01:18:36 +0000

    Wed, Oct 12, 2022, 1:18 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,7 +34,11 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-
+    for (int i = 0; i < 44; i++)
+    {
+        uint8_t 
+        fprintf(output, )
+    }
     // TODO: Read samples from input file and write updated data to output file
 
     // Close files
