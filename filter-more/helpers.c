aa9ada62793852589184e5a0b0de236d2d40d118
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-10-12 19:44:31 +0000

    Wed, Oct 12, 2022, 7:44 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -38,6 +38,13 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE pixel = image[i][j];
+        }
+    }
     return;
 }
 
