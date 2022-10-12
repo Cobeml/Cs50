6a9b38b3b00a32e626c5b38969057cc69888b52a
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-10-12 16:42:45 +0000

    Wed, Oct 12, 2022, 4:42 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -26,7 +26,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         {
             RGBTRIPLE *pixel = image[i][j];
             RGBTRIPLE *reverse = image[i][width - j - 1];
-            RGBTRIPLE buffer = pixel;
+            RGBTRIPLE *buffer = *pixel;
             pixel = reverse;
             reverse = buffer;
         }
