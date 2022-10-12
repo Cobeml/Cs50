8f79698a9ddea987d21f9b2ff0a08bac4f738838
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-10-12 15:59:19 +0000

    Wed, Oct 12, 2022, 3:59 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -5,8 +5,12 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; i < width; i++){
-            BYTE
+        for (int j = 0; i < width; i++)
+        {
+            BYTE gray = round((image[i][j].rgbtBlue + image[i][j].rgbtGreem + image[i][j].rgbtRed) / 3);
+            image[i][j].rgbtBlue = gray;
+            image[i][j].rgbtGreen = gray;
+            image[i][j].rgbtRed = gray;
         }
     }
     return;
