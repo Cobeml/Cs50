4faf1c07edd820acf39e7541f5050008348a2b76
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-10-12 01:48:45 +0000

    Wed, Oct 12, 2022, 1:48 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -40,7 +40,10 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
-    for (int i = 0; )
+    for (int i = 0; *(input + 45 + (2 * i)) != NULL; i ++)
+    {
+        
+    }
     // Close files
     fclose(input);
     fclose(output);
