03e4eb747fd654e8ba022d544748906e1e745892
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-10-12 16:00:40 +0000

    Wed, Oct 12, 2022, 4:00 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -1,4 +1,5 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -7,7 +8,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; i < width; i++)
         {
-            BYTE gray = round((image[i][j].rgbtBlue + image[i][j].rgbtGreem + image[i][j].rgbtRed) / 3);
+            BYTE gray = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3);
             image[i][j].rgbtBlue = gray;
             image[i][j].rgbtGreen = gray;
             image[i][j].rgbtRed = gray;
