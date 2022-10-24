fb69e548670653147421eeaa387532b392a27d4d
Cobeml <113061337+Cobeml@users.noreply.github.com>
2022-10-24 19:26:25 +0000

    Mon, Oct 24, 2022, 7:26 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -92,7 +92,10 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             int kernelVal[] = {-1, 0, 1, -2, 0, 2, -1, 0, 1};
             double Gx = 0;
             double Gy = 0;
-            for (int h = 0; h < )
+            for (int h = 0; h < 3; h++)
+            {
+                for (int k = 0)
+            }
             Gx += image[i - 1][j - 1] * -1;
             Gx += image[i][j - 1] * -2;
             Gx += image[i + 1][j - 1] * -1;
